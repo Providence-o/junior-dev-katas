c609b6d23add2d0077c065db70da82fe7b3f4084
Providence-o <97491889+Providence-o@users.noreply.github.com>
2026-03-23 17:05:12 +0000

    Add parameterised tests to cover gilded rose requirements
    Also adds a minor code change to handle Conjured items
    
    Co-authored-by: Alice Wong <alarthast@users.noreply.github.com>
    Co-authored-by: Katie Bickford <KatieB5@users.noreply.github.com>

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -10,7 +10,7 @@ class GildedRose(object):
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality = item.quality - 1 - int(item.name.startswith("Conjured"))
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -10,3 +10,39 @@ def test_foo():
     gilded_rose.update_quality()
     assert "foo" == items[0].name
 
+@pytest.mark.parametrize(
+    "item,expected_quality",
+    [
+        (Item("foo", 5, 7), 6),
+        (Item("bar", -1, 7), 5),
+        (Item("bar", 4, 0), 0),
+        (Item("Aged Brie", 4, 0), 1),
+        (Item("Aged Brie", 4, 50), 50),
+        (Item("Backstage passes to a TAFKAL80ETC concert", 4, 50), 50),
+        (Item("Backstage passes to a TAFKAL80ETC concert", 11, 20), 21),
+        (Item("Backstage passes to a TAFKAL80ETC concert", 9, 10), 12),
+        (Item("Backstage passes to a TAFKAL80ETC concert", 5, 10), 13),
+        (Item("Backstage passes to a TAFKAL80ETC concert", -1, 10), 0),
+        (Item("Sulfuras, Hand of Ragnaros", 5, 80), 80),
+        (Item("Conjured foo", 11, 20), 18),
+    ],
+)
+def test_item_quality(item, expected_quality):
+    items = [item]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == expected_quality
+
+
+@pytest.mark.parametrize(
+    "item,expected_sell_in",
+    [
+        (Item("Backstage passes to a TAFKAL80ETC concert", 5, 7), 4),
+        (Item("Sulfuras, Hand of Ragnaros", 5, 80), 5),
+    ],
+)
+def test_sell_in(item, expected_sell_in):
+    items = [item]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].sell_in == expected_sell_in
