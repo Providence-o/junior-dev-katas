32404ef641fa835c6ae74ff8da290d8440fd1632
Alice Wong <alice.wong@thedatalab.org>
2026-04-17 11:39:40 +0100

    Refactor backstage passes (1)

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,30 +9,28 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
-            if (
-                item.name != "Aged Brie"
-                and item.name != "Backstage passes to a TAFKAL80ETC concert"
-            ):
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 0:
+                        item.quality = item.quality - item.quality
+            elif item.name != "Aged Brie":
                 if item.quality > 0:
                     item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+                    if item.quality > 0:
+                        item.quality = item.quality - 1
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
