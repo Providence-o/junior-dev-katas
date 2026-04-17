96a0f4e3efa1a54a161b0faa4fc558965011ea9b
Alice Wong <alice.wong@thedatalab.org>
2026-04-17 11:47:30 +0100

    WIP: Brie refactor

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -20,12 +20,12 @@ class GildedRose(object):
                             item.quality = item.quality + 1
                     if item.sell_in < 0:
                         item.quality = item.quality - item.quality
-            elif item.name != "Aged Brie":
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-            else:
+            elif item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality = item.quality + 1
+            else:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
